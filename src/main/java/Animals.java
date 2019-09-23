81b3d34eeb3041bf27e2ff64bb7c040016fa6b56
Tona <tonadaphine77@gmail.com>
2019-09-23 11:28:56 +0200

    Added Animal class

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -1,2 +1,66 @@
-public class Animals {
+import java.util.Objects;
+import org.sql2o.*;
+import java.util.List;
+
+public class Animal {
+    private  String name;
+    private int id;
+
+    //Constants
+
+    public static final String  THREAT_TYPE ="Non-Endangereds";
+    //constructor
+
+    public Animal(String name){ this.name = name; }
+
+    //getters
+
+    public String getName(){ return name; }
+    public int getId(){ return  id;}
+    public static String getThreatType(){ return  THREAT_TYPE  ;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return id == animal.id &&
+                Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name,threattype) VALUES (:name,:threattype)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("threattype", THREAT_TYPE)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Animal> all() {
+        String sql = "SELECT * FROM animals where threattype = 'Non-Endangereds'";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Animal.class);
+        }
+    }
+
+    public static Animal find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals where id=:id";
+            Animal animal = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Animal.class);
+            return animal;
+        }
+    }
+
 }
